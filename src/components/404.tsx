e4d59f7da46f29db30ad303ef57e4800b131ef87
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-12-08 08:38:01 -0500

    feat(NAVIGATION): add 404 error page

---
+++ src/components/404.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Dialogue from "@/components/dialogue";
+
+export default function FourOhFour() {
+  return (
+    <Dialogue
+      title="Santa's AI"
+      messages={["You aren't supposed to be here human !"]}
+    >
+      <></>
+    </Dialogue>
+  );
+}
