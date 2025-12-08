47d1279c6ae27a3c3a969ffdaaff0060bc5ce187
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-12-08 08:43:14 -0500

    refactor(ERRORS): extract error components

---
+++ src/components/errors/malfunction.tsx
@@ -0,0 +1,9 @@
+import Dialogue from "@/components/dialogue";
+
+export default function Malfunction() {
+  return (
+    <Dialogue title="Santa's AI" messages={["Malfunction !"]}>
+      <></>
+    </Dialogue>
+  );
+}
---
+++ src/components/errors/outOfRiddles.tsx
@@ -0,0 +1,12 @@
+import Dialogue from "@/components/dialogue";
+
+export default function OutOfRiddles() {
+  return (
+    <Dialogue
+      title="Santa's AI"
+      messages={["I have been defeated !", " ", "Great job human !"]}
+    >
+      <></>
+    </Dialogue>
+  );
+}
