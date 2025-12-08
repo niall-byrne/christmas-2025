0c09c572b8cd0e14a0c83d92e38f508dc71488fb
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-12-08 08:38:01 -0500

    feat(NAVIGATION): add 404 error page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import FourOhFour from "@/components/404";
+import Background from "@/components/background";
+
+export default async function NotFound() {
+  return (
+    <div>
+      <Background />
+      <FourOhFour />
+    </div>
+  );
+}
