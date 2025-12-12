8463ecc1c0e1002464a8058247eb6abbd361552b
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-12-12 09:06:40 -0500

    feat(BUTTONS): allow enter key to press buttons

--- src/components/buttons/confirmation.tsx
+++ src/components/buttons/confirmation.tsx
@@ -1,8 +1,25 @@
 "use client";
 
 import { Button } from "@chakra-ui/react";
+import { useCallback, useEffect } from "react";
 
 export default function Confirmation({ onClick }: { onClick: () => void }) {
+  const onKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.key === "Enter") {
+        onClick();
+      }
+    },
+    [onClick]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", onKeyDown);
+    return () => {
+      document.removeEventListener("keydown", onKeyDown);
+    };
+  }, [onKeyDown]);
+
   return (
     <Button color={"green"} background={"lightgrey"} onClick={onClick}>
       OK!
