1a4263a3c2d91946b6b52f21aa6e5ba9515feebb
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-12-15 16:20:22 -0500

    feat(FORM): disable autofocus, add placeholder

--- src/components/forms/answer.tsx
+++ src/components/forms/answer.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { Field, Flex, Input } from "@chakra-ui/react";
+import { useRef, useState } from "react";
 import Submit from "@/components/buttons/submit";
-import { useState } from "react";
 
 export default function Answer({
   onGuess,
@@ -11,7 +11,15 @@ export default function Answer({
   onGuess: (guess: string, riddleId: string) => void;
   riddleId: string;
 }) {
+  const inputRef = useRef<HTMLInputElement>(null);
   const [answer, setAnswer] = useState("");
+
+  const startInput = () => {
+    if (inputRef.current) {
+      inputRef.current.placeholder = "";
+      inputRef.current.focus();
+    }
+  };
 
   return (
     <form
@@ -23,11 +31,15 @@ export default function Answer({
       <Flex>
         <Field.Root marginRight={3}>
           <Input
-            autoFocus={true}
+            autoComplete={"off"}
             bg={"white"}
             borderColor={"black"}
             color={"black"}
+            name={"otp"}
             onChange={(e) => setAnswer(e.target.value)}
+            onClick={startInput}
+            placeholder="Click to type a guess!"
+            ref={inputRef}
           />
         </Field.Root>
         <Submit />
